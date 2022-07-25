b5cd5600fb855873b17b4eb3b1a864dbf8120718
RushiKonganapalli <rushikesh.konganapalli@techmojo.in>
2022-07-25 18:02:05 +0530

    done

---
+++ FirstExampleAppllication-1/src/main/java/com/example/demo/FirstExampleAppllication1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstExampleAppllication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstExampleAppllication1Application.class, args);
+	}
+
+}
